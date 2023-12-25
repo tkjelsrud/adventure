c3e72e5a7eaca065cf4bd5746501635a305aed92
tkjelsrud <tkjelsrud@gmail.com>
2023-12-25 10:52:30 +0100

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ const player = {
 };
 
 // Main game loop
-function gameLoop() {
+/*function gameLoop() {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -27,7 +27,48 @@ function gameLoop() {
 
     // Call the next frame
     requestAnimationFrame(gameLoop);
-}
+}*/
 
 // Start the game loop
 gameLoop();
+
+// Handle keyboard input
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'ArrowUp':
+            player.y -= player.speed;
+            break;
+        case 'ArrowDown':
+            player.y += player.speed;
+            break;
+        case 'ArrowLeft':
+            player.x -= player.speed;
+            break;
+        case 'ArrowRight':
+            player.x += player.speed;
+            break;
+    }
+});
+
+function drawWalls() {
+    // Example: Draw back wall
+    const backWallHeight = 200;
+    ctx.fillStyle = 'gray';
+    ctx.fillRect(0, 0, canvas.width, backWallHeight);
+
+    // Example: Draw side walls
+    const sideWallWidth = 50;
+    ctx.fillRect(0, 0, sideWallWidth, canvas.height);
+    ctx.fillRect(canvas.width - sideWallWidth, 0, sideWallWidth, canvas.height);
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawWalls();
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(player.x - 10, player.y - 10, 20, 20);
+
+    requestAnimationFrame(gameLoop);
+}
